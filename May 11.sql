3b5f84b052deea06e8462555418c669e9a5cd87e
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-11 12:58:47 +0545

    Add files via upload

---
+++ May 11.sql
@@ -0,0 +1,103 @@
+select * from FootballPlayers_audit
+
+
+select * from FootballPlayers
+
+
+insert into FootballPlayers
+values
+(42, 'Piotr Zielinski', 'Poland', 'Napoli', 20, 'Midfielder')
+
+
+select * from Players
+
+
+select * from PlayersAudit
+
+
+select * from FManager
+
+
+select * from Cricket
+
+
+select * from HumanResources.Department
+
+
+select * from HumanResources.Employee
+
+
+select JobTitle, Gender, datediff(year, BirthDate, getdate()) as Age, datediff(year, HireDate, getdate()) as Experience
+from HumanResources.Employee
+order by Experience desc
+
+
+select * from HumanResources.EmployeeDepartmentHistory
+
+
+select * from HumanResources.EmployeePayHistory
+
+
+select d.Name, d.GroupName, e.ShiftID, count(*) as Employees
+from HumanResources.Department d
+inner join HumanResources.EmployeeDepartmentHistory e
+on d.DepartmentID = e.DepartmentID
+group by Name, GroupName, ShiftID
+
+
+select string_agg(GroupName, ', ') from HumanResources.Department
+
+
+select row_number() over (order by GroupName) as Rownumber from HumanResources.Department
+
+
+select Name, lead(Name) over (order by DepartmentID) from HumanResources.Department
+
+
+select Name, lag(Name) over (order by DepartmentID) from HumanResources.Department
+
+
+select * from Sales.SalesOrderDetail
+
+
+select SalesOrderID, dense_rank() over (order by UnitPrice) as PriceRank from Sales.SalesOrderDetail
+
+
+select * from Sales.SalesOrderDetail where UnitPrice = (select max(UnitPrice) from Sales.SalesOrderDetail)
+
+
+SELECT value
+FROM STRING_SPLIT('Apple,Orange,Banana', ',')
+
+
+select choose (3, 'This', 'That', 'There', 'Where')
+
+
+select parsename('AdventureWorks.Sales.OrderDetails', 1) as Sales
+
+
+select * from Sales.Customer
+
+
+select TerritoryID, count(*), grouping(TerritoryID) from Sales.Customer 
+group by TerritoryID with rollup
+
+
+select * from Sales.SalesPerson
+
+
+SELECT SalesQuota, SUM(SalesYTD) 'TotalSalesYTD', GROUPING(SalesQuota) 'Grouping'  
+FROM Sales.SalesPerson  
+GROUP BY SalesQuota with rollup
+
+
+select left_shift(12345, 5)
+
+
+select right_shift(12345, 5)
+
+
+select bit_count(0xabc)
+
+
+select @@dbts
