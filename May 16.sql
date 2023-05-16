650c37e17fe2d1865b33a5c5ed3d9e36aca113fb
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-16 12:29:46 +0545

    Add files via upload

--- May 16.sql
+++ May 16.sql
@@ -33,3 +33,41 @@ from ScrapProducts s
 inner join Production.WorkOrder w
 on s.WorkOrderID = w.WorkOrderID
 where StartDate between '2011-07-01' and '2013-07-01'
+
+
+select ProductName, ProductNumber, ProductModel, cast((StandardCost  + ListPrice) as int) as TotalCost 
+from ScrapProductDetails order by TotalCost desc
+
+
+select * from HumanResources.Employee
+
+
+select BusinessEntityID, NationalIDNumber, JobTitle, BirthDate, MaritalStatus, Gender, datediff(year, BirthDate, getdate()) as Age,
+HireDate, datediff(year, HireDate, getdate()) as Experience, VacationHours, SickLeaveHours
+into Emp
+from HumanResources.Employee
+order by Age desc
+
+
+select * from Emp
+
+
+select h.FirstName, h.MiddleName, h.LastName, e.BirthDate, e.Age, e.Gender, e.MaritalStatus, h.JobTitle, e.HireDate, e.Experience,
+h.PhoneNumber, h.AddressLine1, h.City, h.StateProvinceName, h.PostalCode, h.CountryRegionName
+into EmpFullDetails
+from HumanResources.vEmployee h
+inner join Emp e
+on h.BusinessEntityID = e.BusinessEntityID
+
+
+select * from EmpFullDetails
+
+
+select Gender, count(*) as Number from EmpFullDetails group by Gender
+
+
+select concat_ws(' ', FirstName, MiddleName, LastName) as FullName from EmpFullDetails order by FullName
+
+
+select * from EmpFullDetails where Age = (select max(Age) from EmpFullDetails)
+or Experience = (select max(Experience) from EmpFullDetails)
