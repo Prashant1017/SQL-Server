44578edcb28816d5878660743032ebc6ab8f24d8
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-18 16:15:47 +0545

    Add files via upload

--- May 18.sql
+++ May 18.sql
@@ -82,22 +82,13 @@ on p.product_id = o.product_id
 where p.product_id = @product_id
 
 
-execute BikeDetails 1
-
+execute BikeDetails 21
 
 
 select * from sales.order_items
 
 
 select product_id, list_price, discount from sales.order_items order by product_id
-
-
-
-create table Discount
-(
-	product_id int,
-	discount decimal(2,2)
-)
 
 
 select * from sales.order_items where quantity = 1
@@ -107,3 +98,95 @@ select product_id, discount, count(*) from sales.order_items group by product_id
 
 
 select * from sales.order_items
+
+
+select * from sales.customers
+
+
+select * from sales.customers where phone is not null
+
+
+select * from sales.order_items
+
+
+select i.product_id, i.order_id, o.customer_id, o.store_id, o.staff_id, i.quantity, i.list_price, i.discount, o.order_status, o.order_date,	o.required_date, o.shipped_date
+from sales.order_items i
+inner join sales.orders o
+on i.order_id = o.order_id
+
+
+create view [OrderDetails] as
+select i.product_id, i.order_id, o.customer_id, o.store_id, o.staff_id, i.quantity, i.list_price, i.discount, o.order_status, o.order_date,	o.required_date, o.shipped_date
+from sales.order_items i
+inner join sales.orders o
+on i.order_id = o.order_id
+
+
+select * from OrderDetails
+
+
+create view [OrderFullDetails]  as
+select o.order_id, o.product_id, 
+concat_ws(' ' , c.first_name, c.last_name) as CustomerName, c.phone as CustomerNumber, c.email as CustomerMail, concat_ws(',', c.street, c.city) as CustomerAddress,
+s.store_name, s.phone as StoreNumber, s.email as StoreMail, concat_ws(',', s.street, s.city) as StoreAddress,
+concat_ws(' ', st.first_name, st.last_name) as StaffName, st.email as StaffMail, st.phone as StaffNumber, st.store_id
+from OrderDetails o
+inner join sales.customers c on c.customer_id = o.customer_id
+inner join sales.stores s on s.store_id = o.store_id
+inner join sales.staffs st on st.staff_id = o.staff_id
+
+
+select * from OrderFullDetails
+
+
+select * from ProductDetails
+
+
+select * from OrderDetails
+
+
+select concat_ws(' ', c.first_name, c.last_name) as CustomerName, c.phone, p.product_name, s.store_name, o.quantity, o.list_price,
+o.discount, (o.list_price - (o.list_price * o.discount)) as FinalPrice, o.order_status, o.order_date, o.required_date, o.shipped_date
+into CustomerOrders
+from OrderDetails o
+inner join sales.customers c on o.customer_id = c.customer_id
+inner join sales.stores s on o.store_id = s.store_id
+inner join production.products p on o.product_id = p.product_id
+
+
+select * from CustomerOrders
+
+
+select * from CustomerOrders where FinalPrice > 2000
+
+
+select * from CustomerOrders order by FinalPrice desc
+
+
+select store_name, count(*) as Total from CustomerOrders group by store_name
+
+
+select * from sales.orders where shipped_date is null 
+
+
+select * from sales.orders where staff_id = 4
+
+
+select * from sales.staffs
+
+
+select * from OrderFullDetails 
+
+
+update sales.staffs
+set active = 0, store_id = 3, manager_id = null
+where staff_id = 10
+
+
+select distinct staff_id from sales.orders
+
+
+select distinct StaffName from OrderFullDetails
+
+
+execute BikeDetails 222
