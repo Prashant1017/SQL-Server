9a19c58a8bd119d7388cedb09504da3e709f8e31
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-21 15:28:35 +0545

    Add files via upload

--- May 21.sql
+++ May 21.sql
@@ -42,3 +42,52 @@ with cte_numbers(n, weekday) as
 )
 
 select weekday from cte_numbers
+
+
+create table Cats
+(
+	cat_id int,
+	cat varchar(30),
+	ancestor_id int
+)
+
+
+insert into Cats
+values
+(1, 'Felidae', null),
+(2, 'Pantherinae', 1),
+(3, 'Felinae', 1),
+(4, 'Panthera', 2),
+(5, 'Acinonyx', 3),
+(6, 'Acinonyxa', 3),
+(7, 'Leopard', 4),
+(8, 'Lion', 4),
+(9, 'Jaguar', 4),
+(10, 'Snow Leopard', 4),
+(11, 'Tiger', 4),
+(12, 'Cheetah', 5),
+(13, 'Cougar', 6)
+
+
+select * from Cats
+
+
+with RecursiveHierarchy as
+(
+	--	Anchor Member (Starting Point)
+	
+	select cat_id, cat, ancestor_id, 0 as level
+	from Cats
+--	where cat_id = 13
+	union all
+
+	--	Recursive Member
+
+	select c.cat_id, c.cat, c.ancestor_id, level + 1
+	from Cats c
+	inner join RecursiveHierarchy rh
+	on c.cat_id = rh.ancestor_id
+)
+
+select cat_id, cat, ancestor_id, level
+from RecursiveHierarchy
