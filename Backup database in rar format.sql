bdf28e9e788cb203bb78fd4322a671149c30b14b
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-25 12:53:44 +0545

    Add files via upload

--- Backup database in rar format.sql
+++ Backup database in rar format.sql
@@ -1,26 +1,8 @@
-alter procedure sp_generate_compressed_backup
-@DBName varchar(200),
-@Backup_Location varchar(max),
-@SizeOfWinRARFile int,
-@CompressedBackupFileLocation varchar(max)
-as
-begin
-	declare @BackupSQL nvarchar(max)
-	declare @Backup_Name varchar(max)
-	declare @WinRarCommand varchar(5000)
-
-	set @Backup_Name = @DBName + '_' + replace(convert(date, getdate()), '-', '_')
-	set @BackupSQL = 'Backup database [' + @DBName + '] to disk = ''' + @Backup_Location + '\' + @Backup_Name + '.bak''
-	with compression, copy_only'
-	set @WinRarCommand = '"rar.exe" a -v' + convert(varchar, @SizeOfWinRARFile) + 'M ' + @CompressedBackupFileLocation + 
-	'\' + @Backup_Name + '.rar' + @Backup_Location + '\' + @Backup_Name + '.bak'
-	
-	execute sp_executesql @BackupSQL
-	execute xp_cmdshell @Winrarcommand
-end
+execute xp_cmdshell 'C:\"Program Files"\WinRAR\WinRAR.exe a -ep1 -r "D:\Backup\DWDiagnostics.rar" "D:\Backup\DWDiagnostics.bak"'
 
 
-execute sp_generate_compressed_backup 'DWDiagnostics', 'D:\Backup', 10, 'D:\Backup'
+backup database [AdventureWorks2019]
+to disk = 'D:\Backup\AdventureWorks2019.bak'
+with compression, format
 
-
-execute xp_cmdshell 'D:\Backup\winrar-x64-621.exe a -ep1 -r "D:\Backup\DWDiagnostics.rar" "D:\Backup\DWDiagnostics.bak"'
+execute xp_cmdshell 'C:\"Program Files"\WinRAR\WinRAR.exe a "D:\Backup\AdventureWorks2019.rar" "D:\Backup\AdventureWorks2019.bak"'
