2d68c35289bdcfaafd6a28640bf0036eebbe0d3e
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-25 17:22:32 +0545

    Add files via upload

--- Backup database in rar format.sql
+++ Backup database in rar format.sql
@@ -1,8 +1,15 @@
-execute xp_cmdshell 'C:\"Program Files"\WinRAR\WinRAR.exe a -ep1 -r "D:\Backup\DWDiagnostics.rar" "D:\Backup\DWDiagnostics.bak"'
-
+--	Database Backup	--
 
 backup database [AdventureWorks2019]
 to disk = 'D:\Backup\AdventureWorks2019.bak'
 with compression, format
 
+
+--	Backup Database in RAR Format	--
+
 execute xp_cmdshell 'C:\"Program Files"\WinRAR\WinRAR.exe a "D:\Backup\AdventureWorks2019.rar" "D:\Backup\AdventureWorks2019.bak"'
+
+
+--	Deletes Backup Databases In BAK Format	--
+
+execute xp_cmdshell 'del D:\Backup\AdventureWorks2019.bak'
