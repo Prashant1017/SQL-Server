226c2264f44b886e9ae7e30c1302916e886e9e42
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-05-26 12:42:06 +0545

    Add files via upload

--- May 26.sql
+++ May 26.sql
@@ -1,3 +1,5 @@
+--	CTE [Common Table Expressions]	--
+
 select * from Cats
 
 
@@ -38,3 +40,77 @@ select * from Ancestors
 
 
 select * from CatFamily
+
+
+select * from HumanResources.vEmployeeDepartment
+
+
+select Department, count(*) from HumanResources.vEmployeeDepartment group by Department
+
+
+select * from HumanResources.vEmployee
+
+
+select * from HumanResources.vEmployeeDepartmentHistory
+
+
+with DepartmentEmployee as
+(
+	select concat_ws(' ', FirstName, MiddleName, LastName) as FullName, JobTitle
+	from HumanResources.vEmployeeDepartment
+	where Department = 'Production'
+)
+
+select * from DepartmentEmployee
+
+
+with Fibonacci (n, fib_n, next_fib_n) as
+(
+	select 1, 0, 1
+
+	union all
+
+	select n + 1, next_fib_n, fib_n + next_fib_n
+	from Fibonacci where n < 10
+)
+
+select * from Fibonacci
+
+
+with cte as
+(
+	select 1 as n, 1 as p, -1 as q
+
+	union all
+
+	select n + 1, q * 2, p * 2
+	from cte
+	where n < 5
+)
+
+select * from cte
+
+
+with cte (n) as
+(
+	select 1
+
+	union all
+
+	select n + 1
+	from cte
+	where n < 10
+)
+
+select * from cte
+
+
+select * from Sales.SalesOrderDetail
+
+
+select ModifiedDate, LineTotal from Sales.SalesOrderDetail
+
+
+select ModifiedDate, cast(sum(LineTotal) as int) as TotalPrice from Sales.SalesOrderDetail 
+group by ModifiedDate
+order by ModifiedDate
