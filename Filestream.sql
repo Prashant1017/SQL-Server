fe079e2616b506ab0863835ce995b0cf44ad2787
Prashant <104210073+Prashant1017@users.noreply.github.com>
2023-06-01 11:27:19 +0545

    Add files via upload

--- Filestream.sql
+++ Filestream.sql
@@ -27,3 +27,24 @@ select * from Photos
 select * from sys.data_spaces where type = 'FD'
 select * from sys.database_files where type = 2
 select * from sys.configurations where name = 'filestream access level'
+
+
+create table Files
+(
+	ID uniqueidentifier rowguidcol not null unique,
+	Name varchar(20),
+	Files varbinary(max) filestream null
+)
+
+
+declare @file as varbinary(max)
+
+select @file = cast(bulkcolumn as varbinary(max)) from openrowset(bulk 'D:\new.txt', single_blob) as x
+
+insert into Files
+select newid(), 'File-1', @file
+
+
+select * from Files
+
+select * from Photos
